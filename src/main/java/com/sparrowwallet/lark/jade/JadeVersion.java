35e36e03618e6cce334c97fd8983fe997c8b6f06
Shooter <hello@bitcoinshooter.com>
2026-08-21 18:41:59 -0700

    Add files via upload

--- src/main/java/com/sparrowwallet/lark/jade/JadeVersion.java
+++ src/main/java/com/sparrowwallet/lark/jade/JadeVersion.java
@@ -1,7 +1,9 @@
 package com.sparrowwallet.lark.jade;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sparrowwallet.drongo.Version;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record JadeVersion(String JADE_VERSION, int JADE_OTA_MAX_CHUNK, String JADE_CONFIG, String BOARD_TYPE, String JADE_FEATURES,
+public record JadeVersion(Version JADE_VERSION, int JADE_OTA_MAX_CHUNK, String JADE_CONFIG, String BOARD_TYPE, String JADE_FEATURES,
                           String IDF_VERSION, String CHIP_FEATURES, String EFUSEMAC, int BATTERY_STATUS, JadeState JADE_STATE, JadeNetwork JADE_NETWORKS, boolean JADE_HAS_PIN) {
 }
